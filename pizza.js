83d79c6f3212764ba4ff60f1c0a79aa382fdc5c8
santiagorg <yami_n2o@hotmail.com>
2017-07-13 19:50:40 +0200

    all buttons done

---
+++ pizza.js
@@ -0,0 +1,30 @@
+// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+
+	$('.btn-pepperonni').on('click', function(){
+		$('.pep').fadeToggle(1000);
+	});
+
+
+	$('.btn-mushrooms').on('click', function(){
+		$('.mushroom').fadeToggle(1000);
+	});
+
+
+	$('.btn-green-peppers').on('click', function(){
+		$('.green-pepper').fadeToggle(1000);
+	});
+	
+	$('section').removeClass('sauce-white');
+
+	$('.btn-sauce').on('click', function(){
+		$('.sauce').toggleClass('sauce-white');
+	});
+
+	$('section').removeClass('crust-gluten-free');
+
+		$('.btn-crust').on('click', function(){		
+		$('.crust').toggleClass('crust-gluten-free');
+	});
+
+})
